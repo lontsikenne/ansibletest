9502d438fa134786143f4e8e8c940a79f3c97d8e
patrice <patrice>
2020-05-29 17:25:00 -0400

    second commit

--- script.sh
+++ script.sh
@@ -3,4 +3,3 @@ touch /home/oracle/test
 mkdir /home/oracle/folder1
 echo 'hello this is a test of a copy file scripts using ansible' > /home/oracle/test
 cp /home/oracle/test /home/oracle/folder1/
-chown -R oracle:oracle /home/oracle/folder1/
