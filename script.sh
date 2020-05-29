87bef24305d3ed96c96c9b1f3ec607fcbe2a8f9a
patrice <patrice>
2020-05-29 18:38:03 -0400

    wilfreid

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 touch /home/oracle/test
 mkdir /home/oracle/folder1
-echo 'salut bradley commen tu vas?' > /home/oracle/test
+echo 'je parles avec willfreind' > /home/oracle/test
 cp /home/oracle/test /home/oracle/folder1/
